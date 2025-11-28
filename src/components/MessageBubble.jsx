843ca3abf5472d8f95d71b64f31cdeb1e039edb7
PhilipGSanova <sanov2004@gmail.com>
2025-11-28 11:25:22 +0530

    28-11-3

--- src/components/MessageBubble.jsx
+++ src/components/MessageBubble.jsx
@@ -3,14 +3,18 @@ import "./MessageBubble.css";
 
 export default function MessageBubble({ msg, currentUserId }) {
   const senderId = msg.sender?._id || msg.sender?.id || msg.sender;
-  const isSender = senderId === currentUserId;
+const normalizedSenderId = senderId?.toString();
+const normalizedCurrentUserId = currentUserId?.toString();
 
+const isSender = normalizedSenderId === normalizedCurrentUserId;
 
   return (
     <div className={`message-bubble ${isSender ? "sender" : "receiver"}`}>
       <span className="username">{msg.sender.username}: </span>
       <span className="text">{msg.text}</span>
-      <span className="timestamp">{new Date(msg.createdAt).toLocaleTimeString()}</span>
+      <span className="timestamp">
+        {new Date(msg.createdAt).toLocaleTimeString()}
+      </span>
     </div>
   );
 }
