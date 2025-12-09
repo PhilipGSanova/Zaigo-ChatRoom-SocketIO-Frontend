d8b024dd60c5304f16e805f4af9724c330c524e2
PhilipGSanova <sanov2004@gmail.com>
2025-12-09 13:32:15 +0530

    09-12-7

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -90,7 +90,7 @@ export default function ChatWindow({ messages, socket, currentRoom, user, addMes
 
     const formData = new FormData();
     formData.append("roomId", currentRoom._id);
-    formData.append("senderId", user._id);
+    formData.append("senderId", user.id);
     formData.append("attachment", file);
 
     console.log("USER OBJECT =", user);
